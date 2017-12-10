d44f98c67dda4c1772906efdac4c7f86cea6cc39
Nikhil Agarwal <nikhilagarwal0963@gmail.com>
2017-12-10 15:56:54 -0800

    updated mobile header

--- resources/views/include/guide-navbar.blade.php
+++ resources/views/include/guide-navbar.blade.php
@@ -86,60 +86,14 @@
     </div>
 </div>
 
-<div id="guide-navbar-wrapper-mobile" class="carousel slide" data-ride="carousel" data-interval="false">
-    <!-- Indicators -->
-    {{--<ol class="carousel-indicators">--}}
-        {{--<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>--}}
-        {{--<li data-target="#carousel-example-generic" data-slide-to="1"></li>--}}
-        {{--<li data-target="#carousel-example-generic" data-slide-to="2"></li>--}}
-        {{--<li data-target="#carousel-example-generic" data-slide-to="3"></li>--}}
-        {{--<li data-target="#carousel-example-generic" data-slide-to="4"></li>--}}
-    {{--</ol>--}}
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-        <div class="item active">
-            <img src="/images/domain_search.png">
-            <div class="carousel-caption">
-                Select your Domain
-            </div>
-        </div>
-        <div class="item">
-            <img src="/images/hosting.png">
-            <div class="carousel-caption">
-                Setup Hosting
-            </div>
-        </div>
-        <div class="item">
-            <img src="/images/wordpress.png">
-            <div class="carousel-caption">
-                Install WordPress
-            </div>
-        </div>
-        <div class="item">
-            <img src="/images/design.png">
-            <div class="carousel-caption">
-                Design your Blog
-            </div>
-        </div>
-        <div class="item">
-            <img src="/images/launch.png">
-            <div class="carousel-caption">
-                Get Blogging Today
-            </div>
-        </div>
-    </div>
-
-    <!-- Controls -->
-    <a class="left carousel-control" href="#guide-navbar-wrapper-mobile" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#guide-navbar-wrapper-mobile" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
+<div id="guide-navbar-wrapper-mobile">
+    <div class="step active">Step 1</div>
+    <div class="step">Step 2</div>
+    <div class="step">Step 3</div>
+    <div class="step">Step 4</div>
+    <div class="step">Step 5</div>
 </div>
+<div id="guide-navbar-wrapper-mobile-border"></div>
 
 <div id="guide-menu-divider-container">
     <hr id="guide-menu-divider">
